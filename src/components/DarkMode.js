999726f44168386077e64ef400414e2babbafe6b
AcademiccMacharia <machariab19@gmail.com>
2023-05-27 16:17:24 +0300

    [Feature]: Implementing color theme switcher

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -3,16 +3,38 @@ import { ReactComponent as Sun } from './Sun.svg'
 import { ReactComponent as Moon } from './Moon.svg'
 
 const DarkMode = () => {
-  return (
-    <div className='dark_mode'>
-        <input className='light_change_input' type='checkbox' id='darkmode-toggle'
-        />
-        <label className='light_change_label' htmlFor='darkmode-toggle'>
-        <Sun />
-        <Moon />
-        </label>
-    </div>
-  )
-}
 
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('dark-theme', 'dark');
+        localStorage.setItem("selectedTheme", 'dark');
+    }
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('dark-theme', 'light');
+        localStorage.setItem("selectedTheme", 'light');
+    }
+    const toggleTheme = (e) => {
+        if (e.target.checked) setDarkMode();
+        else setLightMode();
+    };
+    const selectedTheme = localStorage.getItem("selectedTheme")
+    if (selectedTheme === 'dark') {
+        setDarkMode();
+    } else {
+        setLightMode();
+    }
+    return (
+        <div className='dark_mode'>
+            <input className='light_change_input'
+                type='checkbox'
+                id='darkmode-toggle'
+                onChange={toggleTheme}
+                defaultChecked={selectedTheme === 'dark'}
+            />
+            <label className='light_change_label' htmlFor='darkmode-toggle'>
+                <Sun />
+                <Moon />
+            </label>
+        </div>
+    )
+}
 export default DarkMode
--- src/components/Search.js
+++ src/components/Search.js
@@ -3,27 +3,27 @@ import { FaSearch } from 'react-icons/fa'
 
 
 const Search = () => {
-    
-const [selectedValue, setSelectedValue] = useState('option1');
 
-const handleSelectChange = (event) => {
-  setSelectedValue(event.target.value);
-};
+    const [selectedValue, setSelectedValue] = useState('option1');
+
+    const handleSelectChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
     return (
         <div className='search-filter'>
             <div className='search'>
                 <input className="search-input" placeholder='Search for a country..' />
                 <FaSearch className='search-icon' />
             </div>
-            
+
             <div className='filter'>
-                <select value={selectedValue} onChange={handleSelectChange} >
-                    <option>Filter By Region</option>
-                    <option value='Africa'>Africa</option>
-                    <option value='America'>America</option>
-                    <option value='Asia'>Asia</option>
-                    <option value='Europe'>Europe</option>
-                    <option value='Oceania'>Oceania</option>
+                <select value={selectedValue} onChange={handleSelectChange} className="filter-select">
+                    <option className="option" hidden >Filter By Region</option>
+                    <option className="option" value='Africa'>Africa</option>
+                    <option className="option" value='America'>America</option>
+                    <option className="option" value='Asia'>Asia</option>
+                    <option className="option" value='Europe'>Europe</option>
+                    <option className="option" value='Oceania'>Oceania</option>
                 </select>
             </div>
         </div>
