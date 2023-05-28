ac9ee54309d4ae64930f77ae352149646424f9a7
AcademiccMacharia <machariab19@gmail.com>
2023-05-28 15:18:30 +0300

    Updates

---
+++ precache-manifest.cf43bdb07726f67d4f89dd6d1186f659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "20943700221f76dbd1b4",
+    "url": "/CountryVibe/static/css/main.6e8da2df.chunk.css"
+  },
+  {
+    "revision": "20943700221f76dbd1b4",
+    "url": "/CountryVibe/static/js/main.20943700.chunk.js"
+  },
+  {
+    "revision": "1de023fcfa004572f73d",
+    "url": "/CountryVibe/static/js/1.1de023fc.chunk.js"
+  },
+  {
+    "revision": "71e0725516cdb6f306c1",
+    "url": "/CountryVibe/static/js/runtime~main.71e07255.js"
+  },
+  {
+    "revision": "686c113d6ce796c4b02614a0f4b5df5d",
+    "url": "/CountryVibe/static/media/Sun.686c113d.svg"
+  },
+  {
+    "revision": "2a40a8319e3f018c0df97141a3aed387",
+    "url": "/CountryVibe/static/media/Moon.2a40a831.svg"
+  },
+  {
+    "revision": "902a74923aee1f0f3ddbfb7262197ef9",
+    "url": "/CountryVibe/index.html"
+  }
+];
