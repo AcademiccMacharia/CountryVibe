8b670e79638bf82fe0d471a4f77d282347588301
AcademiccMacharia <machariab19@gmail.com>
2023-05-28 15:57:16 +0300

    setting data fetching from api to data.json

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,53 +1,92 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
+// import data from './data.json'
 
 
-const countries_url = 'https://restcountries.com/v3.1';
-const Products = ({selectedRegion}) => {
+// // const countries_url = 'https://restcountries.com/v3.1';
+// const countries_url = data;
+// const Products = ({selectedRegion}) => {
 
-  //fetch products from Rest api
-  const [countries, setCountries] = useState([])
+//   //fetch products from Rest api
+//   const [countries, setCountries] = useState([])
 
-  useEffect(() => {
-    const fetchCountries = async () => {
-      try {
-        const res = await fetch(`${countries_url}/all`);
+//   useEffect(() => {
+//     const fetchCountries = async () => {
+//       try {
+//         const res = await fetch(countries_url);
 
-        if (!res.ok) throw new Error('Failed to fetch countries.');
+//         if (!res.ok) throw new Error('Failed to fetch countries.');
 
-        const results = await res.json();
-        setCountries(results);
-      } catch (error) {
-        console.error('Error fetching countries:', error);
-      }
-    };
+//         const results = await res.json();
+//         setCountries(results);
+//       } catch (error) {
+//         console.error('Error fetching countries:', error);
+//       }
+//     };
 
-    fetchCountries();
-  }, []);
+//     fetchCountries();
+//   }, []);
 
-  const filteredCountries = countries.filter((country) =>
-  selectedRegion === 'all' ? true : country.region === selectedRegion
-);
+//   const filteredCountries = countries.filter((country) =>
+//   selectedRegion === 'all' ? true : country.region === selectedRegion
+// );
 
 
-return (
-<div className='products'>
-  {filteredCountries.map(country => ( 
-    <div className='product' key={country.id}>
-      <img src={country.flags.png} alt={country.name.common} />
-      <div className='product-info'>
-        <h3>{country.name.common}</h3>
-        <p>
-                  {" "}
-                  <span>Population: </span>
-                  {new Intl.NumberFormat().format(country.population)}
-                </p>
-        <p className='region'><span>Region: </span>{country.region}</p>
-        <p className='capital'><span>Capital City: </span>{country.capital}</p>
-      </div>
+// return (
+// <div className='products'>
+//   {filteredCountries.map(country => ( 
+//     <div className='product' key={country.id}>
+//       <img src={country.flags.png} alt={country.name.common} />
+//       <div className='product-info'>
+//         <h3>{country.name.common}</h3>
+//         <p>
+//                   {" "}
+//                   <span>Population: </span>
+//                   {new Intl.NumberFormat().format(country.population)}
+//                 </p>
+//         <p className='region'><span>Region: </span>{country.region}</p>
+//         <p className='capital'><span>Capital City: </span>{country.capital}</p>
+//       </div>
+//     </div>
+//   ))}
+// </div>
+// )
+//   }
+
+// export default Products
+
+import React from 'react';
+import data from './data.json';
+
+const Products = ({ selectedRegion }) => {
+  const filteredCountries = data.filter(
+    (country) =>
+      selectedRegion === 'all' || country.region === selectedRegion
+  );
+
+  return (
+    <div className="products">
+      {filteredCountries.map((country) => (
+        <div className="product" key={country.id}>
+          <img src={country.flags.png} alt={country.name.common} />
+          <div className="product-info">
+            <h3>{country.name.common}</h3>
+            <p>
+              <span>Population: </span>
+              {new Intl.NumberFormat().format(country.population)}
+            </p>
+            <p className="region">
+              <span>Region: </span>
+              {country.region}
+            </p>
+            <p className="capital">
+              <span>Capital City: </span>
+              {country.capital}
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
-  ))}
-</div>
-)
-  }
+  );
+};
 
-export default Products
+export default Products;
